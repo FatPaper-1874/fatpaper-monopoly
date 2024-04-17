27af651f100a46f871cb5e8c01f9e343d6662025
FatPaper-1874 <360655753@qq.com>
2024-04-17 16:46:51 +0800

    update

--- global.config.ts
+++ global.config.ts
@@ -1,8 +1,8 @@
-export const FATPAPER_HOST = "fatpaper.site";
-// export const FATPAPER_HOST = "localhost";
+// export const FATPAPER_HOST = "fatpaper.site";
+export const FATPAPER_HOST = "localhost";
 
-export const FATPAPER_LOGIN_PORT = 80;
-export const MONOPOLY_CLIENT_PORT = 81;
+export const MONOPOLY_CLIENT_PORT = 80;
+export const FATPAPER_LOGIN_PORT = 81;
 export const MONOPOLY_ADMIN_PORT = 82;
 export const FATPAPER_USER_SERVER_PORT = 83;
 export const MONOPOLY_SERVER_PORT = 84;
