9ea9e7aa7153100b9aa62795e4969947e9962943
泠曜 <58989046+YipSzeho@users.noreply.github.com>
2024-09-23 00:56:22 +0800

    Update quick-docker-start.sh
    引导代理主机设置；
    修复部分系统无法正确运行Docker Compose的问题。

--- quick-docker-start.sh
+++ quick-docker-start.sh
@@ -1,1 +1,21 @@
-docker compose -f ./docker-compose-local.yml -p fatpapersite up --force-recreate -d --build
+#!/bin/bash
+
+echo "请输入代理主机IP:端口（无需代理可按回车跳过）："
+read proxy_host
+if [ -z "$proxy_host" ]; then
+    echo "未设置代理主机"
+else
+    export http_proxy=$proxy_host
+    export https_proxy=$proxy_host
+    echo "代理主机已临时设置为：$proxy_host"
+fi
+
+docker_compose_path=$(whereis docker-compose | awk '{print $2}')
+if [ -z "$docker_compose_path" ]
+then
+    echo "docker compose -f ./docker-compose-local.yml -p fatpapersite up --force-recreate -d --build"
+    docker compose -f ./docker-compose-local.yml -p fatpapersite up --force-recreate -d --build
+else
+    echo "$docker_compose_path -f ./docker-compose-local.yml -p fatpapersite up --force-recreate -d --build"
+    $docker_compose_path -f ./docker-compose-local.yml -p fatpapersite up --force-recreate -d --build
+fi
