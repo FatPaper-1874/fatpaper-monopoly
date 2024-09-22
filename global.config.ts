1a9976585a1154cfb4cfe285d74fc3a2909cda04
FatPaper-1874 <360655753@qq.com>
2024-09-22 15:44:49 +0800

    同步子模块，进入WebRTC时代
    同步子模块，进入WebRTC时代

--- global.config.ts
+++ global.config.ts
@@ -5,9 +5,8 @@ export const FATPAPER_LOGIN_PORT = 81;
 export const MONOPOLY_ADMIN_PORT = 82;
 export const FATPAPER_USER_SERVER_PORT = 83;
 export const MONOPOLY_SERVER_PORT = 84;
-
-export const MONOPOLY_SOCKET_PORT = 85;
+export const ICE_SERVER_PORT = 85;
 
 export const MYSQL_PORT = 3306;
-export const MYSQL_USERNAME = 'root';
-export const MYSQL_PASSWORD = 'root';
+export const MYSQL_USERNAME = "root";
+export const MYSQL_PASSWORD = "root";
