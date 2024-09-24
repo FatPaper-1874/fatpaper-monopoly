0f9c585fe7760392c4d84d88b0af11db20a9381a
FatPaper-1874 <360655753@qq.com>
2024-09-25 02:37:17 +0800

    优化配置文件global.config.ts, 提高灵活度

--- global.config.ts
+++ global.config.ts
@@ -1,11 +1,29 @@
-export const FATPAPER_HOST = "localhost";
+export const FATPAPER_DOMAIN = "localhost"; //域名: 登录跳转的时候会用到
+export const PROTOCOL = "http"; //协议: http https
+export const USE_PORT = true; //默认启用端口模式
 
 export const MONOPOLY_CLIENT_PORT = 80;
 export const FATPAPER_LOGIN_PORT = 81;
 export const MONOPOLY_ADMIN_PORT = 82;
-export const FATPAPER_USER_SERVER_PORT = 83;
+export const USER_SERVER_PORT = 83;
 export const MONOPOLY_SERVER_PORT = 84;
 export const ICE_SERVER_PORT = 85;
+
+//路径定义: 使用nginx映射的时候会用到
+export const USER_SERVER_PATH = "user-server";
+export const MONOPOLY_SERVER_PATH = "monopoly-server";
+export const ICE_SERVER_PATH = "ice-server";
+
+export const FATPAPER_LOGIN_URL = `${PROTOCOL}://${FATPAPER_DOMAIN}${USE_PORT ? ":" : "/"}${FATPAPER_LOGIN_PORT}`;
+export const USER_SERVER_URL = `${PROTOCOL}://${FATPAPER_DOMAIN}${
+	USE_PORT ? `:${USER_SERVER_PORT}` : `/${USER_SERVER_PATH}`
+}`;
+export const MONOPOLY_SERVER_URL = `${PROTOCOL}://${FATPAPER_DOMAIN}${
+	USE_PORT ? `:${MONOPOLY_SERVER_PORT}` : `/${MONOPOLY_SERVER_PATH}`
+}`;
+export const ICE_SERVER_URL = `${PROTOCOL}://${FATPAPER_DOMAIN}${
+	USE_PORT ? `:${ICE_SERVER_PORT}` : `/${ICE_SERVER_PATH}`
+}`;
 
 export const MYSQL_PORT = 3306;
 export const MYSQL_USERNAME = "root";
